e2cd411d762387a37a71141993607ae600e88a23
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2024-12-11 15:03:28 +0200

    add pages folder and files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,35 @@
 import '../index.css';
 import 'modern-normalize';
-import ContactList from './ContactList/ContactList';
-import ContactForm from './ContactForm/ContactForm';
-import SearchBox from './SearchBox/SearchBox';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchContacts } from '../redux/contacts/operations';
-import { selectIsError, selectIsLoading } from '../redux/contacts/selectors';
-// import { Route, Routes } from 'react-router-dom';
+// import ContactList from './ContactList/ContactList';
+// import ContactForm from './ContactForm/ContactForm';
+// import SearchBox from './SearchBox/SearchBox';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { fetchContacts } from '../redux/contacts/operations';
+// import { selectIsError, selectIsLoading } from '../redux/contacts/selectors';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const isError = useSelector(selectIsError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectIsLoading);
+  // const isError = useSelector(selectIsError);
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
+  // return (
+  //   <div>
+  //     <h1>Phonebook</h1>
+  //     <ContactForm />
+  //     <SearchBox />
+  //     {isLoading && <h2>Loading...</h2>}
+  //     {isError && <h2>Oops something went wrong</h2>}
+  //     <ContactList />
+  //   </div>
+  // );
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      {isLoading && <h2>Loading...</h2>}
-      {isError && <h2>Oops something went wrong</h2>}
-      <ContactList />
-    </div>
+    <Routes>
+      <Route></Route>
+    </Routes>
   );
-  // <Routes>
-  //   <Route></Route>
-  // </Routes>;
 };
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
-      {/* <BrowserRouter>
-      </BrowserRouter> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
