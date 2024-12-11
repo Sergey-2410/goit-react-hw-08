7272dd6c736c0cb0a3ed925cceaabe5e5bf92f59
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2024-12-11 15:44:48 +0200

    add folders and files

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,17 @@
+// import { Field, Form, Formik } from 'formik';
+
+const LoginForm = () => {
+  return (
+    <div>
+      log in
+      {/* <Formik>
+        <Form>
+          <Field></Field>
+          <Field></Field>
+        </Form>
+      </Formik> */}
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,18 @@
+// import { Field, Form, Formik } from 'formik';
+
+const RegistrationForm = () => {
+  return (
+    <div>
+      registration
+      {/* <Formik>
+        <Form>
+          <Field></Field>
+          <Field></Field>
+          <Field></Field>
+        </Form>
+      </Formik> */}
+    </div>
+  );
+};
+
+export default RegistrationForm;
