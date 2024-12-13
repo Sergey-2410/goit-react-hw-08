7f10660bab6ad370e9f8fd74f33bc4149284b867
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2024-12-13 03:34:33 +0200

    add persistor

--- src/main.jsx
+++ src/main.jsx
@@ -2,40 +2,43 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
-        <Toaster
-          position="top-right"
-          reverseOrder={false}
-          gutter={8}
-          containerClassName=""
-          containerStyle={{}}
-          toastOptions={{
-            // Define default options
-            className: '',
-            duration: 5000,
-            style: {
-              background: '#363636',
-              color: '#fff',
-            },
-
-            // Default options for specific types
-            success: {
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+          <Toaster
+            position="top-right"
+            reverseOrder={false}
+            gutter={8}
+            containerClassName=""
+            containerStyle={{}}
+            toastOptions={{
+              // Define default options
+              className: '',
               duration: 5000,
-              theme: {
-                primary: 'green',
-                secondary: 'black',
+              style: {
+                background: '#363636',
+                color: '#fff',
               },
-            },
-          }}
-        />
+
+              // Default options for specific types
+              success: {
+                duration: 5000,
+                theme: {
+                  primary: 'green',
+                  secondary: 'black',
+                },
+              },
+            }}
+          />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,38 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts/slice';
 import { filtersReducer } from './filters/slice';
 import { authReducer } from './auth/slice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'contacts',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
     contacts: contactsReducer,
     filters: filtersReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
